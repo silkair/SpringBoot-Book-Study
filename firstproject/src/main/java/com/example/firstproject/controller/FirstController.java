985f2b35e89ac129175daad16e16c5e54901b3f2
silkair <silkair1234@gmail.com>
2024-11-11 21:34:37 +0900

    feat(controller) : url 연결 요청 추가

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,10 +1,13 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
+    @GetMapping("/hi") //url 요청 접수
+
     public String NiceToMeetYou() {
-        return "greetings"; // greetings.mutache 파일 반환
+        return "greeting"; // greeting.mustache 파일 반환
     }
 }
