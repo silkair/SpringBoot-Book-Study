1e97a326d7dc85908f3e3c49367abb6345bdd8b8
silkair <silkair1234@gmail.com>
2024-11-11 21:48:18 +0900

    feat(controller): bye 만들어보기

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -2,12 +2,18 @@ package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class FirstController {
     @GetMapping("/hi") //url 요청 접수
-
-    public String NiceToMeetYou() {
+    public String NiceToMeetYou(Model model) {
+        model.addAttribute("username", "이친당");
         return "greeting"; // greeting.mustache 파일 반환
+    }
+
+    @GetMapping("/bye")
+    public String SeeYouNext(Model model) {
+        return "goodbye";
     }
 }
