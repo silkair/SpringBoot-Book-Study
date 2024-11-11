ae87b1262ecf829663de91d017b859c5ea9d1561
silkair <silkair1234@gmail.com>
2024-11-12 03:26:09 +0900

    feat(repository): 레포지토리 생성 및 연결

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -2,12 +2,17 @@ package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    @Autowired //스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입(DI)
+    private ArticleRepository articleRepository;
+
     @GetMapping("/articles/new")
     public String newArticleFrom() {
         return "articles/new";
@@ -19,6 +24,7 @@ public class ArticleController {
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
         // 2. 리파지터리로 엔티티를 DB에 저장
+        Article saved = articleRepository.save(article);
         return "";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -9,9 +9,24 @@ import jakarta.persistence.Id;
 public class Article {
     @Id
     @GeneratedValue
-    private long id;
+    private Long id;
     @Column
     private String title;
     @Column
     private String content;
+
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
