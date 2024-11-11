e8e8ebf1dde41a420be16a612faf51faff5683da
silkair <silkair1234@gmail.com>
2024-11-11 23:48:40 +0900

    feat(controller): 폼 데이터 받기

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -2,11 +2,17 @@ package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
     @GetMapping("/articles/new")
     public String newArticleFrom() {
         return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle() {
+        return "";
     }
 }
