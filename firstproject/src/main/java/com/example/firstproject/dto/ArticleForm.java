dd0605c6dfc08fa1b056fce6916fd60e2e23087f
silkair <silkair1234@gmail.com>
2024-11-14 20:54:35 +0900

    refactor: use lambok

--- firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,23 +1,15 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     private String title;
     private String content;
     //전송받은 제목과 내용을 필드에 저장하는 생성자 추가
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-    //데이터를 잘 받았는지 확인할 toString()
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title,content);
